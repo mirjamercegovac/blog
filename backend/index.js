a6d3321fb5330ed60b600f04b89bebc1ba96f44f
Mirjam Ercegovac <mirjam.ercegovac@gmail.com>
2024-03-18 14:19:31 +0100

    Add project

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+// const app = express();
+// const mysql = require("mysql");
+
+// const db = mysql.createConnection({
+//     user: "root",
+//     host: "localhost",
+//     password: "password", 
+//     database: "",
+// });
+
+// app.get("/", (req, res) => {
+//     res.json("yes");
+// })
+
+// app.listen(8800, () => {
+//     console.log("Connected to backend!");
+// });
